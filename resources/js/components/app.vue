b158860bbc4e157bcd4815f50af4323fd3e6d63f
Artem Nikulin <itsacats@gmail.com>
2022-07-24 14:26:13 +0300

    initial commit

---
+++ resources/js/components/app.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1 class="h1">Hello from Vue Laravel project!</h1>
+    <button @click="hello">Click</button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+
+    methods: {
+        hello() {
+            alert('Hey!')
+        }
+    },
+
+    computed: {
+
+    },
+
+    mounted() {
+
+    }
+}
+</script>
+
